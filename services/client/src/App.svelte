3538375109de9bbcbc357006c672b45eca3ab9f1
lebrancconvas <lebrancconvas@gmail.com>
2022-06-25 11:34:59 +0700

    ✨ Create Landing Page UI.

--- services/client/src/App.svelte
+++ services/client/src/App.svelte
@@ -1,65 +1,21 @@
-<script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+<script>
+
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <header>
+    <div class="heading">
+      <h1>Nhee Memo | จดบันทึกตารางการค้างชำระค่าใช้จ่ายของคุณ</h1>
+    </div>
+    <div class="catchphrase">
+      <h3>ให้คุณไม่ต้องภวงค์ว่ายังลืมค้างค่าชำระอะไรบ้าง ด้วย Nhee Memo</h3> 
+    </div>
+  </header>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
   main {
     text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+    font-family: 'Prompt';
   }
 </style>
